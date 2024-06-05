0995323b7d421c1467e16ae7ced1786f61a67ae3
Richard Canoa <richard.canoa@uab.edu.bo>
2024-06-04 22:22:56 -0400

    Agregar archivos de proyecto.

---
+++ BackendDemo1/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendDemo1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
