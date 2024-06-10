013a6023d3472ca1c95f2b716fbe532df7356bbb
Richard Canoa <richard.canoa@uab.edu.bo>
2024-06-10 18:49:05 -0400

    creacion de clases del dbcontext

---
+++ BackendDemo1/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BackendDemo1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendDemo1.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(
+            DbContextOptions<AppDbContext> options) :
+            base(options)
+        {
+            
+        }
+        DbSet<Person> Persons { get; set; }
+    }
+}
